3bc9baaad9507946b3ab6f4195284367e5df1358
Eray Alp <erayalp.e@gmail.com>
2023-06-24 18:13:12 +0300

    editForm added

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import TodoForm from './TodoForm'
 import {v4 as uuidv4} from 'uuid'
 import Todo from './Todo';
+import EditTodoForm from './EditTodoForm';
 uuidv4();
 
 function TodoWrapper() {
@@ -36,19 +37,30 @@ function TodoWrapper() {
             {...todo, isEditing: !todo.isEditing} : todo
         ))
     }
+
+    const editTask = (task, id) => {
+        setTodos(todos.map(todo => 
+            (todo.id === id) ?
+            {...todo, task: task, isEditing: !todo.isEditing} : todo
+        ))
+    }
     
   return (
     <div className='TodoWrapper'>
         <h1>Let's Plan All Out!</h1>
         <TodoForm addTodo={addTodo}/>
         {todos.map((todo, index) => (
-            <Todo 
-            task={todo} 
-            key={index} 
-            toggleComplete={toggleComplete}
-            deleteTodo={deleteTodo}
-            editTodo={editTodo}
-            />
+            todo.isEditing ? (
+                <EditTodoForm editTask={editTask} todo={todo}/>
+            ) : (
+                <Todo 
+                todo={todo} 
+                key={index} 
+                toggleComplete={toggleComplete}
+                deleteTodo={deleteTodo}
+                editTodo={editTodo}
+                />
+            )
         ))}
     </div>
   )
