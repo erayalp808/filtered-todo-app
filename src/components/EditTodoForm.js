59c4ee0cefb3efb725d0c2e1782943fa05fe7994
Eray Alp <erayalp.e@gmail.com>
2023-06-24 18:12:41 +0300

    editForm created

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function EditTodoForm() {
+function EditTodoForm({editTask, todo}) {
+    const [value, setValue] = useState(todo.task);
+
+    const handleSubmit = e => {
+        // prevent default action
+        e.preventDefault();
+        if (value) {
+          // edit todo
+          editTask(value, todo.id);
+          // clear form after submission
+          setValue('');
+        }
+    }
   return (
-    <div>
-        
-    </div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        className='todo-input' 
+        placeholder='Update task'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        />
+
+        <button 
+        type='submit' 
+        className='todo-btn'
+        >Update</button>
+    </form>
   )
 }
 
