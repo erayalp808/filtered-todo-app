56a7a34d526eef92356a87d7e32b8660c4af86ee
Eray Alp <erayalp.e@gmail.com>
2023-06-24 17:09:44 +0300

    bug fixed

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TodoForm from './TodoForm'
 import {v4 as uuidv4} from 'uuid'
 import Todo from './Todo';
@@ -8,13 +8,13 @@ function TodoWrapper() {
     const [todos, setTodos] = useState([]);
 
     const addTodo = todo => {
-        setTodos(...todos, 
+        setTodos([...todos, 
         {
             id: uuidv4(), 
             task: todo, 
             completed: false, 
             isEditing: false
-        });
+        }]);
     }
     
   return (
